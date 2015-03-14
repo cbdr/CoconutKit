07063814b7d9952405ad59893410fe010ca8ad01
Samuel Défago <defagos@gmail.com>
2015-03-14 11:48:21 +0100

    Add nullable, remove unnecessary semantics specifiers

--- CoconutKit/Sources/Animation/HLSObjectAnimation.h
+++ CoconutKit/Sources/Animation/HLSObjectAnimation.h
@@ -4,11 +4,15 @@
 //  Licence information is available from the LICENCE file.
 //
 
+#import "HLSNullability.h"
+
 #import <Foundation/Foundation.h>
 
 /**
  * Common abstract class for animations
  */
+
+NS_ASSUME_NONNULL_BEGIN
 @interface HLSObjectAnimation : NSObject <NSCopying>
 
 /**
@@ -17,3 +21,4 @@
 + (instancetype)animation;
 
 @end
+NS_ASSUME_NONNULL_END
