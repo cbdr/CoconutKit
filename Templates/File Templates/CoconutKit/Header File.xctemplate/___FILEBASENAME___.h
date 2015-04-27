4e93404d163ae8daae0122af8e5e3a318f532f70
Samuel Défago <defagos@gmail.com>
2015-04-27 13:32:14 +0200

    Remove include guard

--- Templates/File Templates/CoconutKit/Header File.xctemplate/___FILEBASENAME___.h
+++ Templates/File Templates/CoconutKit/Header File.xctemplate/___FILEBASENAME___.h
@@ -3,9 +3,3 @@
 //
 //  Licence information is available from the LICENCE file.
 //
-
-#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
-#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
-
-
-#endif
